aed0c97b5f8c8be0a4a45a7867247d36adbdccad
elquimiquito <whiteheaded12@gmail.com>
2022-11-17 15:11:13 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -23,12 +23,14 @@ public class Converter {
     }
 
     public static float euroToDollar(float value) {
-        float rsl = value * 7 / 6;
+        float conversionCoefficient = 1.16F;
+        float rsl = value * conversionCoefficient;
         return rsl;
     }
 
      public static float dollarToEuro(float value) {
-        float rsl = value  * 6 / 7;
+        float conersionCoefficient = 0.85F;
+        float rsl = value  * conersionCoefficient;
         return rsl;
     }
 
@@ -41,9 +43,40 @@ public class Converter {
         System.out.println("5 dollars are " + ruble + " ruble.");
         ruble = Converter.euroToRuble(4);
         System.out.println("4 euros are " + ruble + " ruble");
-        dollar = Converter.euroToDollar(10);
-        System.out.println("10 euros are " + dollar + " dollar");
-        euro = Converter.dollarToEuro(15);
-        System.out.println("15 dollars are " + euro + " euro");
+        dollar = Converter.euroToDollar(1);
+        System.out.println("1 euro are " + dollar + " dollar");
+        euro = Converter.dollarToEuro(1);
+        System.out.println("1 dollars are " + euro + " euro");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        in = 120;
+        expected = 2;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("120 rubles are 2. Test result : " + passed);
+        in = 2;
+        expected = 140;
+        out = Converter.euroToRuble(in);
+        passed = expected == out;
+        System.out.println("2 euros are 140. Test result : " + passed);
+        in = 2;
+        expected = 120;
+        out = Converter.rubleToEuro(in);
+        passed = expected == out;
+        System.out.println("2 dollars are 120. Test result : " + passed);
+        in = 1;
+        expected = 1.16F;
+        out = Converter.dollarToEuro(in);
+        passed = expected == out;
+        System.out.println("1 dollars are 0.85. Test result : " + passed);
+        in = 1;
+        expected = 0.85F;
+        out = Converter.euroToDollar(in);
+        passed = expected == out;
+        System.out.println("1 euros are 0.85. Test result : " + passed);
+
     }
 }
