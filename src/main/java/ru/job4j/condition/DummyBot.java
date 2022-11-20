98d466a3453985ad342fd80eb5b7933eeb776077
elquimiquito <whiteheaded12@gmail.com>
2022-11-21 00:31:11 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -5,13 +5,13 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            rsl = "Hi, SmartAss";
+            rsl = "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
-            rsl = "See you later";
+            rsl = "See you later.";
         }
         return rsl;
     }
-    
+
     public static void main(String[] args) {
         String rsl = DummyBot.answer("Hi, Bot.");
         System.out.println(rsl);
