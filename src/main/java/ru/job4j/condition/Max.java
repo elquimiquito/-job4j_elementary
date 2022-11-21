aae4f350d742dc2d8f983d51aca29a616f51084d
elquimiquito <whiteheaded12@gmail.com>
2022-11-21 12:43:34 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
+    public static int max(int left, int right)
         if (right == left) {
             return left;
         } else {
