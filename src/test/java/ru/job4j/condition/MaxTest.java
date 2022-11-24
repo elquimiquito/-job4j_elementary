b6fe3047fa2bb8f84a07886ac7f70cd9186687b1
elquimiquito <whiteheaded12@gmail.com>
2022-11-25 00:43:15 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax2To1Then2() {
         int left = 1;
         int right = 2;
         int result = Max.max(left, right);
@@ -14,7 +14,7 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax3To2Then2() {
+    public void whenMax2To3Then3() {
         int left = 3;
         int right = 2;
         int result = Max.max(left, right);
