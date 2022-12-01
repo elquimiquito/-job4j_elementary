4e64bc4410e21c9f82979a83fc64347cdabfc747
elquimiquito <whiteheaded12@gmail.com>
2022-12-01 17:46:51 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int res = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (res >= coins[i]) {
-                res -= coins[i];
-                rsl[size] = coins[i];
+        for (int coin : coins) {
+            while (res >= coin) {
+                res -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
