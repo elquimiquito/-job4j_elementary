87ed3645342024d30bf731c2ebe1687f35b9b474
elquimiquito <whiteheaded12@gmail.com>
2022-12-08 19:16:29 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -6,34 +6,28 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void when00to11then1dot41() {
+        Point first = new Point(0, 0);
+        Point second = new Point(1, 1);
+        double out = first.distance(second);
         double expected = 1.41;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 1;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when00to03then3() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 3);
+        double out = first.distance(second);
         double expected = 3;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when00to68then10() {
+        Point first = new Point(0, 0);
+        Point second = new Point(6, 8);
+        double out = first.distance(second);
         double expected = 10;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 6;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
